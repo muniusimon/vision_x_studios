d89621b46cb6e897af63e8c52e06283eb79ecc90
muniusimon <muniusimon@gmail.com>
2024-10-10 02:43:47 +0300

    New Project

---
+++ visionxstudio/routes.py
@@ -0,0 +1,123 @@
+import os
+import secrets
+from PIL import Image
+from flask import Flask, render_template, url_for, flash, redirect, request
+from visionxstudio import app, db, bcrypt
+from visionxstudio.forms import RegistrationForm, LoginForm, UpdateAccountForm, ProjectForm
+from visionxstudio.models import User, Post, Project
+from flask_login import login_user, current_user, logout_user, login_required
+
+posts = [
+    {
+        'author': 'Simon Kinuthia',
+        'title': "My first post",
+        'content': 'First Post content',
+        'dateposted': 'April 3, 2023'
+
+    },
+    {
+        'author': 'Kinuthia',
+        'title': "My second post",
+        'content': 'I am who I am',
+        'dateposted': 'April 3, 2024'
+
+    }
+
+    ]
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+
+    return render_template('home.html', title ="home")
+
+@app.route("/about")
+def about ():
+    return render_template("about.html", title = 'About')
+
+@app.route("/contact")
+def contact ():
+    return render_template("contact.html")
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register ():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User (username= form.username.data, email = form.email.data, password = hashed_password )
+        db.session.add(user)
+        db.session.commit()
+        flash ('You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login ():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get ('next')
+            return redirect(next_page) if next_page else redirect (url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email or password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout ():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext (form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profle_pics', picture_fn)
+    form_picture.save (picture_path)
+
+    return picture_fn 
+
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account ():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f'Your account has been update to {form.username.data}', 'success')
+        return redirect (url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename = "images/" + current_user.image_file)
+    return render_template('account.html', title = "account", image_file = image_file, form = form) 
+
+@app.route("/project/new", methods=['GET', 'POST'])
+@login_required
+def new_project():
+    form = ProjectForm()
+    if form.validate_on_submit():
+        project = Project(name=form.name.data, description=form.description.data, author=current_user)
+        db.session.add(project)
+        db.session.commit()
+        flash('Your project has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_project.html', title='New Project', form=form)
+
+@app.route("/projects")
+@login_required
+def projects():
+    projects = Project.query.filter_by(author=current_user).all()
+    return render_template('projects.html', title='Your Projects', projects=projects)
